215c25ab93d7569281caca0c81ef116dad9c1b56
vikash129 <74494285+vikash129@users.noreply.github.com>
2022-02-22 15:59:16 +0530

    fdg

--- virtual_creation.cpp
+++ virtual_creation.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
+
 class CWH{
     protected:
-        string title[30];
+        string title;
         float rating;
     public:
-
         CWH(string s,float rating){
-            strcpy(s,title);
+            this->title = s;
             this->rating = rating ;
+        };  
+      virtual  void display(){
+cout << "CWH base class display()\n";
         };
-        virtual void display();
 };
 
-class Video : public CWH {
+class Video  : public CWH{
     float length ;
   public:
   
-  Video(string s , float r , float vl) : CWH(s , r){
+  Video(string s , float r , float vl) : CWH(s , r) {
       length = vl;
       
   };
@@ -28,30 +30,62 @@ class Video : public CWH {
       cout << "title = " << title << endl ;
             cout << "rating = " << rating << endl ;
                   cout << "length = " << length << endl<<endl ;
-  }
+  };
+ 
+};
+
+class TEXT  : public CWH{
+    int words;
+  public:
+  
+  TEXT(string s , float r , float vl) : CWH(s , r) {
+      words = vl;
+      
+  };
+  void display(){
+      cout << "\nin TEXT Class\n";
+      cout << "title = " << title << endl ;
+            cout << "rating = " << rating << endl ;
+                  cout << "words = " << words << endl<<endl ;
+  };
  
 };
 
 int main()
 {
-    string title = new char[30];
+    string title ;
     float rating,length ;
+    int words;
     
     
-    
-    Video( "" , 4.9 ) ;
- 
-    
-    DerivedC derived_C;
-    DerivedC *ptr_derived_C;
-    
-    ptr_baseA = &derived_C;
-    
-// ptr_derived_C->derived_var = 200;
-// ptr_derived_C->base_var = 300;
-// ptr_derived_C->display();
+    title = "django videos";
+    rating = 4.9;
+    length = 4.8 ; 
 
+    Video djvideo(  title , rating , length ) ;
+    djvideo.display();
 
- ptr_baseA->display(); //with virtual it will call the derived_C members otherwise it will have default behaviour
+       title = "django videos textual";
+    words = 499;
+    length = 4.12 ;
+    TEXT djText(title, rating ,words);
+    djText.display(); 
+
+    CWH *tuts[2];
+
+    tuts[0] = &djvideo ;
+    tuts[1] = &djText;
+    cout <<"via pointer called\n";
+    tuts[0]->display();
+    tuts[1]->display();
+
     return 0;
 }
+
+//rules for virtual functio
+
+//1,the cannot be static
+//2.they are accessd by object pointers
+//3. virtual function bcan be a friend of another class
+//4. a virtual function in base class might not be used.
+//5. if a  virtual func is defined in the base class there is no necessity of rendering it in the derived class.
